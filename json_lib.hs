7218547ef02237bcfd4f2785c6ea0a019daea8a4
Leonardomotta <leonardo.mota@ccc.ufcg.edu>
2018-11-29 18:26:12 -0200

    parser para string e bool

--- json_lib.hs
+++ json_lib.hs
@@ -1,10 +1,12 @@
 import Data.Typeable
-import Data.List.Split
+import Control.Applicative
+import Control.Monad
+import Text.ParserCombinators.Parsec hiding ((<|>),many)
 
-
+{-Json-}
 {-Conjunto de valores possiveis para o Json-}
 data Jvalue  =   Js String | Ji Integer | Jb Bool | Jf Float | Jsa [String] |
- Jia [Integer] | Jba [Bool] | Jfa [Float]| Jn |Jo Json deriving (Show, Eq, Ord)
+ Jia [Integer] | Jba [Bool] | Jfa [Float]| Jn |Jo Json | Joa [Json] deriving (Show, Eq, Ord)
 {-Par chave e valor Para atributos Json-}
 data Jatribute = Jatribute (String , Jvalue) deriving (Show, Eq, Ord)
 {-Objeto Json , composto de um array de atributos-}
@@ -40,8 +42,45 @@ getArrayBool (Jba b) = b
 getArrayFloat :: Jvalue -> [Float]
 getArrayFloat (Jfa f) = f
 
+{-fim do Json-}
+
+
+
+{-Faz parse de bool -}
+matchFalse :: Parser String {- valor falso retorna Right false , c.c erro-}
+matchFalse = string "false"
+matchTrue :: Parser String
+matchTrue = string "true" {-Mesma coisa do anterior-}
+
+alwaysFalse :: Parser Bool {- retorna sempre um parsed bool do false-}
+alwaysFalse = pure False
+alwaysTrue :: Parser Bool {-mesmo que o anterior-}
+alwaysTrue = pure True
+
+boolFalse :: Parser Bool
+boolFalse =  matchFalse *> alwaysFalse {-se match false não lançar erro retorna um parser bool-}
+boolTrue :: Parser Bool
+boolTrue = matchTrue *> alwaysTrue {-mesmo q o anterior porem para true-}
+
+bool :: Parser Bool
+bool = boolTrue<|>boolFalse {-tenta com o valor true cc false-}
+
+jsonBool :: Parser Jvalue
+jsonBool = Jb <$> bool {-mapeia a função bool para o texto lido do  parser-}
+{-fim do parse para bool -}    
+
+
+{-retorna o valor literal lido-}
+stringLiteral :: Parser String
+stringLiteral = char '"' *> (many (noneOf ['"']))<* char '"'
+
+
+jstring :: Parser Jvalue
+jstring = Js <$> stringLiteral
 
 
 
 
 
+
+{-jsonObject = 0 <$> ((char '{')*>(Jvalue `sepBy` (char ','))<*(char '}'))-}
